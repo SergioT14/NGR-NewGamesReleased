0558e45f67a711cde6cedfe55e77a1a0592ac1a4
SergioT14 <93717547+SergioT14@users.noreply.github.com>
2022-02-01 20:09:49 +0100

    Añadidas pestañas y navegación simple
    Se han añadido las pestañas inicio, etiquetas y buscar para su futuro uso.
    En cada pestaña se han creado además dos botones para las otras pestañas, con una caja de texto en el de buscar para probar la funcionalidad de botones con texto para búsquedas dentro de la página web.
    
    ERROR CONOCIDO: La pestaña de inicio al volver desde un botón de las otras no coincide con la de inicio de la web, por lo que genera un error.

--- NewGamesRealeased/src/main/java/com/example/practica/controlador/Controlador.java
+++ NewGamesRealeased/src/main/java/com/example/practica/controlador/Controlador.java
@@ -1,14 +1,37 @@
 package com.example.practica.controlador;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class Controlador {
-
-	@GetMapping("/mustache")
-	public String saludoMustache() {
+	
+	@GetMapping
+	public String inicio(Model modelo) {
 		
-		return "miplantilla";
+		modelo.addAttribute("tipo","inicio");
+		modelo.addAttribute("contenido","los posts más recientes");
+		
+		return "PlantillaInicio";
+	}
+
+	@GetMapping("/etiquetas")
+	public String etiquetas(Model modelo) {
+		
+		modelo.addAttribute("tipo","etiquetas");
+		modelo.addAttribute("contenido","todas las etiquetas para buscar de manera filtrada");
+		
+		return "PlantillaEtiquetas";
+	}
+	
+	@GetMapping("/buscar")
+	public String buscar(Model modelo, @RequestParam String texto) {
+		
+		modelo.addAttribute("tipo","búsqueda");
+		modelo.addAttribute("what",texto);
+		modelo.addAttribute("contenido","los resultados de la búsqueda realizada");
+		
+		return "PlantillaBuscar";
 	}
 }

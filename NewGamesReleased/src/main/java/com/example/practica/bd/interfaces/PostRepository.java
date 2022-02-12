8576ea67ec6189777bfa73934aaadfe0a207fc90
Daniel Alfonsel García <98475235+Danigaralfo@users.noreply.github.com>
2022-02-12 18:05:07 +0100

    Borrado del anterior directorio

--- NewGamesReleased/src/main/java/com/example/practica/bd/interfaces/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.practica.bd.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.practica.bd.entities.*;
-
-public interface PostRepository extends JpaRepository<Post,Long>{
-	
-}

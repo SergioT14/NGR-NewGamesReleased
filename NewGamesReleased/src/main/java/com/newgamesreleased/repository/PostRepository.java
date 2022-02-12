38a9942f4c9e800bee6d17202cb75fdd5d66b847
SergioT14 <93717547+SergioT14@users.noreply.github.com>
2022-02-12 17:33:35 +0100

    Ajuste a la nueva estructura

--- NewGamesReleased/src/main/java/com/newgamesreleased/repository/PostRepository.java
+++ NewGamesReleased/src/main/java/com/newgamesreleased/repository/PostRepository.java
@@ -2,7 +2,7 @@ package com.newgamesreleased.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.newgamesreleased.model.*;
+import com.newgamesreleased.model.Post;
 
 public interface PostRepository extends JpaRepository<Post,Long>{
 	

8c4c082c64e3059650f911be83e2ee408c4c01f4
SergioT14 <93717547+SergioT14@users.noreply.github.com>
2022-02-12 17:34:14 +0100

    Ajuste a la nueva estructura

--- NewGamesReleased/src/main/java/com/newgamesreleased/repository/RatingRepository.java
+++ NewGamesReleased/src/main/java/com/newgamesreleased/repository/RatingRepository.java
@@ -1,7 +1,8 @@
-package com.example.practica.bd.interfaces;
+package com.newgamesreleased.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.practica.bd.entities.*;
+
+import com.newgamesreleased.model.Rating;
 
 public interface RatingRepository extends JpaRepository<Rating,Long>{
 	

c0f15e720212f7499fc299e8452b7e88474455fa
SergioT14 <93717547+SergioT14@users.noreply.github.com>
2022-04-28 11:51:38 +0200

    Cache distribuida
    Añadido apartado de configuración de Hazelcast para tener caché distribuida para que funcione el login con el balanceador de carga

--- NewGamesReleased/src/main/java/com/newgamesreleased/NewGamesRealeasedApplication.java
+++ NewGamesReleased/src/main/java/com/newgamesreleased/NewGamesRealeasedApplication.java
@@ -1,5 +1,8 @@
 package com.newgamesreleased;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -8,6 +11,9 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 
 @SpringBootApplication
@@ -41,4 +47,17 @@ public class NewGamesRealeasedApplication {
 		return rabbitTemplate;
 	}
 
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		JoinConfig jConfig = config.getNetworkConfig().getJoin();
+		jConfig.getMulticastConfig().setEnabled(false);
+		//Lista de servidores para balancear la carga
+		//ampliable si se añaden más al haproxy
+		List<String> instanciasWeb = new ArrayList<>();
+		instanciasWeb.add("ngr-app-1");
+		instanciasWeb.add("ngr-app-2");
+		jConfig.getTcpIpConfig().setEnabled(true).setMembers(instanciasWeb);
+		return config;
+	}
 }
